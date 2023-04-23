3a1ee5adbfdec4e16aec9c93b9890c24b566be6d
Ezequiel Aguilera <ezequielagui102@gmail.com>
2023-04-23 12:59:54 -0400

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './css/App.css'
+import Navbar from './Navbar'
+import Login from './Login'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <div className='container'>
+        <Login/>
+      </div>
+    </>
+  )
+}
+
+export default App
