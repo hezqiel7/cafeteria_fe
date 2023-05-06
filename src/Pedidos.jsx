7008b5bb5a89d1210ed5f0e1f93adce9749e2c14
Ezequiel Aguilera <ezequielagui102@gmail.com>
2023-05-05 22:38:45 -0400

    Agregar nuevo pedido

--- src/Pedidos.jsx
+++ src/Pedidos.jsx
@@ -2,125 +2,248 @@ import { useEffect, useState } from "react";
 import Pedido from "./Pedido";
 import Productos from "./Productos";
 
-function Pedidos({accesstoken}) {
-  const [pedidos, setPedidos] = useState(null);
-  const [mostrarlistos, setMostrarListos] = useState(null)
+function Pedidos({ accesstoken }) {
+  const [pedidos, setPedidos] = useState([]);
+  const [mostrarlistos, setMostrarListos] = useState(null);
   const [mostrarNuevo, setMostrarNuevo] = useState(false);
-  const [productosElegidos, setProductosElegidos] = useState([])
+  const [productosElegidos, setProductosElegidos] = useState([]);
+  const [totalPrecio, setTotalPrecio] = useState(0);
+  const [nombreCliente, setNombreCliente] = useState(null);
+  const [mesa, setMesa] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/pedidos/", {
       headers: {
-        Authorization: "Bearer " + accesstoken
-      }
+        Authorization: "Bearer " + accesstoken,
+      },
     })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.code == "token_not_valid") {
-        sessionStorage.clear();
-        // Renovar token
-      }
-      setPedidos(data);
-    });
-  }, [])
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.code == "token_not_valid") {
+          sessionStorage.clear();
+          // Renovar token
+        }
+        setPedidos(data);
+      });
+  }, []);
 
   const handleClickNuevo = () => {
     setMostrarNuevo(!mostrarNuevo);
   };
 
   const handleCLickQuitar = (e) => {
-    const producto_id = e.target.parentElement.getAttribute("data-producto-id")
-    let arr = [...productosElegidos]
-    const indice = arr.findIndex(p => p.id == producto_id)
-    arr.splice(indice, 1)
-    setProductosElegidos(arr)
-  }
+    const producto_id = e.target.parentElement.getAttribute("data-producto-id");
+    let arr = [...productosElegidos];
+    const indice = arr.findIndex((p) => p.id == producto_id);
+    const precio = arr[indice].precio;
+    const cantidad = arr[indice].cantidad;
+    setTotalPrecio(totalPrecio - precio * cantidad);
+    arr.splice(indice, 1);
+    setProductosElegidos(arr);
+  };
+
+  const handleChangeCantidad = (e) => {
+    const producto_id = e.target.getAttribute("data-producto-id");
+    let arr = [...productosElegidos];
+    const producto = arr.find((p) => p.id == producto_id);
+    producto.cantidad = e.target.value;
+    setProductosElegidos(arr);
+
+    setTotalPrecio(calcularPrecioTotal());
+  };
+
+  const calcularPrecioTotal = () => {
+    let sum = 0;
+    productosElegidos.forEach((producto) => {
+      sum += producto.precio * producto.cantidad;
+    });
+    return sum;
+  };
 
   const handleEnviarPedido = () => {
+    let lista_productos = [];
 
-  }
+    productosElegidos.forEach((producto) => {
+      lista_productos.push({
+        producto_id: producto.id,
+        cantidad: parseInt(producto.cantidad),
+      });
+    });
+
+    // Buscar el id mas grande actualmente
+    const idMasGrande = pedidos.reduce((anterior, actual) => {
+      return actual.id > anterior.id ? actual : anterior;
+    });
+
+    const data = {
+      id: idMasGrande.id + 1,
+      mesa: parseInt(mesa),
+      listo: false,
+      fecha_pedido: new Date(Date.now()).toISOString(),
+      lista_productos: lista_productos,
+      total_precio: totalPrecio,
+      nombre_cliente: nombreCliente,
+    };
+
+    console.log(data);
+
+    fetch("http://localhost:8000/pedidos/", {
+      headers: {
+        Authorization: "Bearer " + accesstoken,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
 
   return (
     <div className="container">
       <div className="d-flex py-2 justify-content-between align-items-center">
-        {!mostrarNuevo ?
+        {!mostrarNuevo ? (
           <>
             <div className="form-check form-switch display-6 fs-5">
-              <input className="form-check-input" type="checkbox" role="switch" id="chkMostrarListos" onChange={e => setMostrarListos(e.target.checked)}/>
-              <label className="form-check-label" htmlFor="chkMostrarListos">Mostrar pedidos listos</label>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="chkMostrarListos"
+                onChange={(e) => setMostrarListos(e.target.checked)}
+              />
+              <label className="form-check-label" htmlFor="chkMostrarListos">
+                Mostrar pedidos listos
+              </label>
             </div>
-            <button className="btn btn-primary" onClick={handleClickNuevo}>Nuevo</button>    
+            <button className="btn btn-primary" onClick={handleClickNuevo}>
+              Nuevo
+            </button>
           </>
-        :
+        ) : (
           <>
-            <button className="btn btn-success" onClick={handleEnviarPedido}>Enviar pedido</button>
-            <button className="btn btn-secondary" onClick={handleClickNuevo}>Cancelar</button>
+            <button className="btn btn-success" onClick={handleEnviarPedido}>
+              Enviar pedido
+            </button>
+            <p>₲ {totalPrecio}</p>
+            <button className="btn btn-secondary" onClick={handleClickNuevo}>
+              Cancelar
+            </button>
           </>
-        }
+        )}
       </div>
-        {!mostrarNuevo ?
-          <div className="d-flex gap-3 justify-content-center flex-wrap mt-3">
-            {pedidos && pedidos.map(pedido => (
-                mostrarlistos ?
-                  <Pedido
-                    key={pedido.id}
-                    accesstoken={accesstoken}
-                    id={pedido.id}
-                    fecha={pedido.fecha_pedido}
-                    mesa={pedido.mesa}
-                    precio={pedido.total_precio}
-                  />
-                :
-                  !pedido.listo ?
-                    <Pedido
-                      key={pedido.id}
-                      accesstoken={accesstoken}
-                      id={pedido.id}
-                      fecha={pedido.fecha_pedido}
-                      mesa={pedido.mesa}
-                      precio={pedido.total_precio}
-                      listo={pedido.listo}
-                    /> 
-                  :
-                    null
-            ))}
-          </div>
-        :
-          <div className="d-flex justify-content-between flex-column flex-md-row">
-            {productosElegidos.length > 0 &&
-              <div className="w-100 p-3">
-                <div className="list-group list-group-flush">
-                  {productosElegidos.map(producto => {
-                    return(
-                      <a className="list-group-item px-4" key={producto.id}>
-                        <div className="d-flex w-100 justify-content-between align-items-center">
-                          <h6 className="m-0 p-0">{producto.nombre}</h6>
-                          <div className="d-flex justify-content-between w-25 gap-3 align-items-center">
-                            <input type="number" className="form-control"/>
-                            <div id="botonQuitar" onClick={handleCLickQuitar} data-producto-id={producto.id}><i className="fa-solid fa-trash-can" style={{color: "#931515"}}></i></div>
+      {!mostrarNuevo ? (
+        <div className="d-flex gap-3 justify-content-center flex-wrap mt-3">
+          {pedidos &&
+            pedidos.map((pedido) =>
+              mostrarlistos ? (
+                <Pedido
+                  key={pedido.id}
+                  accesstoken={accesstoken}
+                  id={pedido.id}
+                  fecha={pedido.fecha_pedido}
+                  mesa={pedido.mesa}
+                  precio={pedido.total_precio}
+                />
+              ) : !pedido.listo ? (
+                <Pedido
+                  key={pedido.id}
+                  accesstoken={accesstoken}
+                  id={pedido.id}
+                  fecha={pedido.fecha_pedido}
+                  mesa={pedido.mesa}
+                  precio={pedido.total_precio}
+                  listo={pedido.listo}
+                />
+              ) : null
+            )}
+        </div>
+      ) : (
+        <div className="d-flex justify-content-between flex-column flex-md-row">
+          {productosElegidos.length > 0 && (
+            <div className="w-100 p-3">
+              <div className="list-group list-group-flush">
+                {productosElegidos.map((producto) => {
+                  return (
+                    <a className="list-group-item px-4" key={producto.id}>
+                      <div className="d-flex w-100 justify-content-between align-items-center">
+                        <h6 className="m-0 p-0">{producto.nombre}</h6>
+                        <div className="d-flex justify-content-between w-25 gap-3 align-items-center">
+                          <input
+                            type="number"
+                            className="form-control"
+                            onChange={handleChangeCantidad}
+                            value={
+                              productosElegidos.find(
+                                (p) => p.id === producto.id
+                              ).cantidad
+                            }
+                            data-producto-id={producto.id}
+                            min="1"
+                          />
+                          <div
+                            id="botonQuitar"
+                            onClick={handleCLickQuitar}
+                            data-producto-id={producto.id}
+                          >
+                            <i
+                              className="fa-solid fa-trash-can"
+                              style={{ color: "#931515" }}
+                            ></i>
                           </div>
                         </div>
-                      </a>
-                    )    
-                    })}
-                </div>
+                      </div>
+                    </a>
+                  );
+                })}
               </div>
-            }
-              <div className="py-3 w-100 p-3">
-                {/* <div className="form-floating mb-4">
-                  <input type="text" className="form-control" id="nombre" placeholder="Nombre"/>
-                  <label htmlFor="nombre" className="form-label-sm text-secondary">Nombre</label>
-                </div> */}
-                <Productos
-                  accesstoken={accesstoken}
-                  productosElegidos={productosElegidos}
-                  setProductosElegidos={setProductosElegidos}
+            </div>
+          )}
+          <div className="py-3 w-100 p-3">
+            <div className="row">
+              <div className="form-floating mb-4 col-9">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="nombre"
+                  placeholder="Nombre"
+                  onChange={(e) => setNombreCliente(e.target.value)}
                 />
+                <label
+                  htmlFor="nombre"
+                  className="form-label-sm text-secondary ms-3"
+                >
+                  Nombre del cliente
+                </label>
               </div>
+              <div className="form-floating mb-4 col-3">
+                <input
+                  type="number"
+                  id="mesa"
+                  className="form-control"
+                  placeholder="Mesa"
+                  onChange={(e) => setMesa(e.target.value)}
+                />
+                <label
+                  htmlFor="mesa"
+                  className="form-label-sm text-secondary ms-3"
+                >
+                  Mesa
+                </label>
+              </div>
+            </div>
+            <Productos
+              accesstoken={accesstoken}
+              productosElegidos={productosElegidos}
+              setProductosElegidos={setProductosElegidos}
+              totalPrecio={totalPrecio}
+              setTotalPrecio={setTotalPrecio}
+            />
           </div>
-        }
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Pedidos;
--- src/Productos.jsx
+++ src/Productos.jsx
@@ -1,45 +1,91 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
-function Productos({accesstoken, productosElegidos, setProductosElegidos}) {
+function Productos({
+  accesstoken,
+  productosElegidos,
+  setProductosElegidos,
+  totalPrecio,
+  setTotalPrecio,
+}) {
+  const productos = useRef(null);
+  const [listaProductos, setListaProductos] = useState(null);
+  const [buscar, setBuscar] = useState(null);
 
-    const [productos, setProductos] = useState(null)
+  useEffect(() => {
+    fetch("http://localhost:8000/productos/", {
+      headers: {
+        Authorization: "Bearer " + accesstoken,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        productos.current = data;
+        setListaProductos(data);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch("http://localhost:8000/productos/", {
-            headers: {
-                Authorization: "Bearer " + accesstoken
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            setProductos(data)
-        })
-    }, [])
+  const handleClickProducto = (producto) => {
+    let arr = [...productosElegidos];
+    arr.push({
+      id: producto.id,
+      nombre: producto.nombre,
+      precio: producto.precio,
+      cantidad: 1,
+    });
+    setProductosElegidos(arr);
+    setTotalPrecio(totalPrecio + producto.precio);
+  };
 
-    const handleClickProducto = (e) => {
-        let producto_id = parseInt(e.target.getAttribute("producto_id"))
-        let producto_nombre = e.target.innerHTML
+  const handleChangeBuscar = (e) => {
+    const data = e.target.value;
+    setBuscar(data);
+    let lista_productos = productos.current.filter((p) => {
+      // Normalizar para ignorar cualquier tilde
+      let nombre = p.nombre
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+      let buscado = data
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+      return nombre.includes(buscado);
+    });
 
-        let arr = [...productosElegidos]
-        arr.push({id:producto_id, nombre:producto_nombre})
-        setProductosElegidos(arr)
-    }
+    setListaProductos(lista_productos);
+  };
 
-    return(
-        <ul className="list-group gap">
-            {productos && productos.map(producto => (
-                <li
-                    className={"list-group-item list-group-item-action list-group-item-info "
-                                .concat(productosElegidos.find(p => p.id === producto.id) ? "disabled" : "")}
-                    key={producto.id}
-                    producto_id={producto.id}
-                    onClick={handleClickProducto}
-                >
-                    {producto.nombre}
-                </li>
-            ))}
-        </ul>
-    )
+  return (
+    <>
+      <div className="input-group rounded mb-2">
+        <input
+          type="search"
+          className="form-control"
+          placeholder="Buscar"
+          onChange={handleChangeBuscar}
+        />
+        <span className="input-group-text border-0">
+          <i className="fas fa-search"></i>
+        </span>
+      </div>
+      <ul className="list-group gap">
+        {listaProductos &&
+          listaProductos.map((producto) => (
+            <li
+              className={"list-group-item list-group-item-action list-group-item-info ".concat(
+                productosElegidos.find((p) => p.id === producto.id)
+                  ? "disabled"
+                  : ""
+              )}
+              key={producto.id}
+              onClick={() => handleClickProducto(producto)}
+            >
+              {producto.nombre}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
 
-export default Productos
+export default Productos;
