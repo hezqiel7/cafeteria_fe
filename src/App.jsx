f0249ca54b2b899cb16877afd1e9b665e3c4941a
Ezequiel Aguilera <ezequielagui102@gmail.com>
2023-05-07 16:08:08 -0400

    Hacer funcionar el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import './css/App.css'
-import { useState } from 'react'
-import Navbar from './Navbar'
-import Login from './Login'
-import Pedidos from './Pedidos'
+import "./css/App.css";
+import { useState } from "react";
+import Navbar from "./Navbar";
+import Login from "./Login";
+import Pedidos from "./Pedidos";
 
 function App() {
+  const [actualtab, setActualTab] = useState("inicio");
 
-  let accesstokenx = localStorage.getItem("accesstoken")
-  let refreshtokenx = localStorage.getItem("refreshtoken")
+  let accesstokenx = localStorage.getItem("accesstoken");
+  let refreshtokenx = localStorage.getItem("refreshtoken");
 
-  if(!accesstokenx || accesstokenx == "undefined") {
-    accesstokenx = sessionStorage.getItem("accesstoken")
-    refreshtokenx = sessionStorage.getItem("refreshtoken")
+  if (!accesstokenx || accesstokenx == "undefined") {
+    accesstokenx = sessionStorage.getItem("accesstoken");
+    refreshtokenx = sessionStorage.getItem("refreshtoken");
   }
 
-  const [accesstoken, setAccesstoken] = useState(accesstokenx)
-  const [refreshtoken, setRefreshtoken] = useState(refreshtokenx)
+  const [accesstoken, setAccesstoken] = useState(accesstokenx);
+  const [refreshtoken, setRefreshtoken] = useState(refreshtokenx);
 
   if (accesstoken && accesstoken != "undefined") {
     return (
       <>
-        <Navbar setAccesstoken={setAccesstoken}/>
-        <Pedidos accesstoken={accesstoken}/>
+        <Navbar setAccesstoken={setAccesstoken} setActualTab={setActualTab} />
+        {actualtab === "pedido" && <Pedidos accesstoken={accesstoken} />}
       </>
-    )
+    );
   }
 
   return (
-    <div className='container h-100 d-flex justify-content-center align-items-center'>
-      <Login setAccesstoken={setAccesstoken}/>
+    <div className="container h-100 d-flex justify-content-center align-items-center">
+      <Login setAccesstoken={setAccesstoken} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,40 +1,75 @@
-function Navbar(props) {
+function Navbar({ setAccesstoken, setActualTab }) {
+  const handleLogOut = (e) => {
+    localStorage.clear();
+    sessionStorage.clear();
+    setAccesstoken(null);
+  };
 
-    const handleLogOut = (e) => {
-        localStorage.clear()
-        sessionStorage.clear()
-        props.setAccesstoken(null)
-    }
-
-    return (
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">App Cafetería</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" onClick={handleLogOut} href="#">Pedidos</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" onClick={handleLogOut} href="#">Productos</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" onClick={handleLogOut} href="#">Usuarios</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" onClick={handleLogOut} href="#">Cerrar sesión</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>        
-    )
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          App Cafetería
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                onClick={() => setActualTab("inicio")}
+                href="#"
+              >
+                Inicio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                onClick={() => setActualTab("pedido")}
+                href="#"
+              >
+                Pedidos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                onClick={() => setActualTab("productos")}
+                href="#"
+              >
+                Productos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                onClick={() => setActualTab("usuarios")}
+                href="#"
+              >
+                Usuarios
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={handleLogOut} href="#">
+                Cerrar sesión
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
